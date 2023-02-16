e2fbeef400fa5b16fb15828f2696f076c62e33bf
erdifree <123300447+erdifree@users.noreply.github.com>
2023-02-16 19:14:33 +0100

    Erdi

--- fake_store/src/components/Cart.js
+++ fake_store/src/components/Cart.js
@@ -7,29 +7,7 @@ const Cart = ({ cart, decrementCart, incrementCart }) => {
   const toggleShowList = () => {
     setShowList(!showList);
   };
-  ////////////////////////////////////////////
-  const [items, setItems] = useState([]);
-  const [totalQuantity, setTotalQuantity] = useState(0);
-
-  const addToCart = (item) => {
-    const newItems = [...items];
-    const existingItemIndex = newItems.findIndex((i) => i.id === item.id);
-    if (existingItemIndex !== -1) {
-      newItems[existingItemIndex].quantity += item.quantity;
-    } else {
-      newItems.push(item);
-    }
-    setItems(newItems);
-    setTotalQuantity(totalQuantity + item.quantity);
-  };
-
-  const removeFromCart = (item) => {
-    const newItems = items.filter((i) => i.id !== item.id);
-    setItems(newItems);
-    setTotalQuantity(totalQuantity - item.quantity);
-  };
-
-  ////////////////////////////////////////////
+ 
   let content = <></>;
   if (showList) {
     content = (
