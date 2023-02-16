3280d485dfbbaced8b9acf0349de8dda976e5a55
MarcoAlemanni <auto13alemanni@gmail.com>
2023-02-16 21:16:05 +0100

    marco

--- fake_store/src/components/Cart.js
+++ fake_store/src/components/Cart.js
@@ -1,30 +1,48 @@
 import { useState } from "react";
-import { BsFillCartFill, BsFillTrashFill } from "react-icons/bs";
+import { BsFillCartFill, BsCart2 } from "react-icons/bs";
+import "./product.css";
 
 const Cart = ({ cart, decrementCart, incrementCart }) => {
   const [showList, setShowList] = useState(false);
 
   const toggleShowList = () => {
-    setShowList( !showList);
+    setShowList(!showList);
   };
- 
+
+  function calculateTotal() {
+    let total = 0;
+    for (let item of cart) {
+      total += item.price * item.quantity;
+    }
+    return total.toFixed(2);
+  }
+
   let content = <></>;
   if (showList) {
     content = (
-      <div>
-        <ul>
-          {cart.map((el) => {
-            return (
-              <li key={el.id}>
-                {el.title} {el.price}
-                <button onClick={() => decrementCart(el)}>-</button>(
-                {el.quantity})
-                <button onClick={() => incrementCart(el)}>+</button>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <ul>
+        {cart.map((el) => {
+          return (
+            <li key={el.id}>
+              <div className="row">
+                <div className="col-6">
+                  {el.title} {el.price}
+                </div>
+                <div className="col-6 text-end ">
+                  <button className="btnCart" onClick={() => decrementCart(el)}>
+                    -
+                  </button>
+                  {el.quantity}
+                  <button className="btnCart" onClick={() => incrementCart(el)}>
+                    +
+                  </button>
+                </div>
+              </div>
+              <hr />
+            </li>
+          );
+        })}
+      </ul>
     );
   }
 
@@ -35,6 +53,10 @@ const Cart = ({ cart, decrementCart, incrementCart }) => {
         <span className="badge bg-secondary">{cart.length}</span>
       </button>
       {content}
+      <div className="text-end">
+        Total <BsCart2 />: {calculateTotal()}
+        <hr />
+      </div>
     </div>
   );
 };
